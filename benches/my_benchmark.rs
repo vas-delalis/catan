3ec4b393ad1fe11e5277d9484c329e0002261055
vas-delalis <vas.delalis@gmail.com>
2025-07-14 16:25:14 +0300

    feat: set up benchmarking

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,40 @@
+use catan::*;
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::seq::IndexedRandom;
+
+struct Agent {}
+
+impl Agent {
+    // pub fn new(&self, initial_obs: InitialObservation) {}
+
+    pub fn get_action(&self, obs: Observation) -> Action {
+        *obs.actions.choose(&mut rand::rng()).unwrap()
+    }
+}
+
+fn play() {
+    let mut state = State::default();
+    let agent = Agent {};
+    while !state.is_terminal() {
+        let action = agent.get_action(state.observe(state.current_player()));
+        // dbg!(state.current_player(), state.phase, action);
+        state.apply_action(action);
+        // agent.update();
+        // sleep(Duration::from_millis(400));
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Full game");
+    group.throughput(criterion::Throughput::Elements(1));
+    group.bench_function("W/ longest road", |b| b.iter(|| play()));
+    group.finish();
+
+    // let mut group = c.benchmark_group("");
+    // group.throughput(criterion::Throughput::Elements(1));
+    // group.bench_function("No longest road", |b| b.iter(|| play()));
+    // group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
