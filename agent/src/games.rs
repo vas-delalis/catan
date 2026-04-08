f944e8f12436fd9e61b4fcd72942cab70ce90301
vas-delalis <vas.delalis@gmail.com>
2026-04-08 22:23:17 +0300

    refactor: rename file

--- agent/src/games.rs
+++ agent/src/games.rs
@@ -1,10 +1,10 @@
 mod dots_and_boxes;
+mod odds_game;
 mod tic_tac_toe;
-mod value_game;
 
 pub use dots_and_boxes::{
     Dir as DotsAndBoxesDir, DotsAndBoxes, Edge as DotsAndBoxesAction, Player as DotsAndBoxesPlayer,
     ScoreEvaluator, TopsideEvaluator,
 };
+pub use odds_game::{NormalizedOddsEvaluator, OddsEvaluator, OddsGame};
 pub use tic_tac_toe::{Cell, TicTacToe, TicTacToePlayer};
-pub use value_game::{NormalizedOddsEvaluator, OddsEvaluator, OddsGame};
