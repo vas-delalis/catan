65fc7b2d0512c33be57c4eeb8d8d631ed527ff04
vas-delalis <vas.delalis@gmail.com>
2026-04-21 22:28:14 +0300

    feat: batching in training

--- agent/src/bin/train.rs
+++ agent/src/bin/train.rs
@@ -2,10 +2,11 @@ use agent::ml::{TrainingConfig, train};
 
 fn main() {
     train(TrainingConfig {
-        epochs: 20,
-        train_iters: 1000,
-        test_iters: 100,
-        replay_count: 1000,
+        epochs: 50,
+        train_replays: 2000,
+        test_replays: 200,
+        batch_size: 10,
         learning_rate: 1e-3,
+        search_evals: 100,
     });
 }
--- agent/src/ml/training.rs
+++ agent/src/ml/training.rs
@@ -1,22 +1,24 @@
-use rand::seq::IteratorRandom;
+use crate::{
+    Tournament,
+    agents::{ConstantEvaluator, Search},
+    games::DotsAndBoxes,
+    ml::{self, Batch, data::Dataset, model::ModelEvaluator},
+};
+use itertools::Itertools;
 use tch::{
     Tensor,
     nn::{self, OptimizerConfig},
 };
 
-use crate::{
-    Agent, Tournament,
-    agents::{ConstantEvaluator, Search},
-    games::DotsAndBoxes,
-    ml::{self, Batch, data::Dataset, model::ModelEvaluator},
-};
+type GAME = DotsAndBoxes;
 
 pub struct TrainingConfig {
     pub epochs: usize,
     pub learning_rate: f64,
-    pub train_iters: usize,
-    pub test_iters: usize,
-    pub replay_count: usize,
+    pub train_replays: usize,
+    pub test_replays: usize,
+    pub batch_size: usize,
+    pub search_evals: usize,
 }
 
 pub fn train(config: TrainingConfig) {
@@ -27,12 +29,26 @@ pub fn train(config: TrainingConfig) {
         .build(&vs, config.learning_rate)
         .unwrap();
 
-    let model = ml::create_model::<DotsAndBoxes>(&root, 16);
+    let model = ml::create_model::<GAME>(&root, 32);
     let evaluator = ModelEvaluator { model: &model };
-    let agent = Search::new(evaluator.clone(), 100, false, 1.41, 1.0, 0.01);
-    let reference_agent = Search::new(ConstantEvaluator { c: 0.0 }, 100, false, 1.41, 1.0, 0.01);
+    let agent = Search::new(
+        evaluator.clone(),
+        config.search_evals,
+        false,
+        1.41,
+        1.0,
+        0.01,
+    );
+    let reference_agent = Search::new(
+        ConstantEvaluator { c: 0.0 },
+        config.search_evals,
+        false,
+        1.41,
+        1.0,
+        0.01,
+    );
 
-    let mut tournament: Tournament<DotsAndBoxes> = Tournament::new(1e-1, 1e-1);
+    let mut tournament: Tournament<GAME> = Tournament::new(0.1, 0.1);
     tournament.add(Box::new(agent.clone()), true);
     tournament.add(Box::new(reference_agent.clone()), true);
     tournament.add(Box::new(reference_agent.clone()), false);
@@ -41,21 +57,39 @@ pub fn train(config: TrainingConfig) {
     tournament.leaderboard();
 
     let params: usize = vs.trainable_variables().iter().map(|t| t.numel()).sum();
-    println!("Training {} parameters", params);
+    println!("Training {} parameters...", params);
 
     for epoch in 1..=config.epochs {
-        let mut dataset_train = Dataset::new(config.replay_count);
-        let mut dataset_test = Dataset::new(config.test_iters);
+        let mut dataset_train = Dataset::new(config.train_replays);
+        let mut dataset_test = Dataset::new(config.test_replays);
         dataset_train.selfplay(&agent);
         dataset_test.selfplay(&agent);
-
         // Train
-        for (state, value) in dataset_train {
-            let x = state.batch();
-            let y = Tensor::from(value as f32);
-            let loss = model(&x).mse_loss(&y, tch::Reduction::Mean);
+        let mut train_losses = vec![];
+        for batch in &dataset_train.into_iter().chunks(config.batch_size) {
+            let mut loss = Tensor::from(0f32);
+            for (state, value) in batch {
+                let x = state.batch();
+                let y = Tensor::from(value as f32);
+                let output = model(&x);
+                loss += output.mse_loss(&y, tch::Reduction::Mean);
+                // dbg!(&x);
+                // dbg!(&y);
+                // let output: f32 = output.try_into().unwrap();
+                // dbg!(output);
+            }
             opt.backward_step(&loss);
+            let loss: f32 = loss.try_into().unwrap();
+            train_losses.push(loss);
         }
+
+        let n = train_losses.len();
+        let sum: f32 = train_losses.iter().sum();
+        println!(
+            "[Train - Epoch {}] Loss {:.3}",
+            epoch,
+            sum / n as f32 / config.batch_size as f32
+        );
 
         // Test
         let _no_grad = tch::no_grad_guard(); // Turn off gradient computation
@@ -73,7 +107,7 @@ pub fn train(config: TrainingConfig) {
         println!("[Test - Epoch {}] Loss {:.3}", epoch, sum / n as f32);
     }
 
-    let mut tournament: Tournament<DotsAndBoxes> = Tournament::new(1e-2, 1e-2);
+    let mut tournament: Tournament<GAME> = Tournament::new(0.05, 0.05);
     tournament.add(Box::new(agent.clone()), true);
     tournament.add(Box::new(reference_agent.clone()), true);
     tournament.add(Box::new(reference_agent.clone()), false);
