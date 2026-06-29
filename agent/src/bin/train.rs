fffaca0112e4e817df9cec7eddbdb11bf5c274d3
vas-delalis <vas.delalis@gmail.com>
2026-06-29 21:42:05 +0300

    refactor: move training config to stable location

--- agent/src/bin/train.rs
+++ agent/src/bin/train.rs
@@ -2,7 +2,12 @@ use agent::ml::{TrainingConfig, train};
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config_json = fs::read_to_string("training_config.json")?;
+    let mut path = common::PROJECT_DIRS.config_dir().to_path_buf();
+    path.push("training_config.json");
+    let config_json = fs::read_to_string(&path).expect(&format!(
+        "A training_config.json file should exist at {}",
+        path.parent().unwrap().to_string_lossy()
+    ));
     let config: TrainingConfig = serde_json::from_str(&config_json)?;
 
     agent::with_game!(config.model_config.game.as_str() => G {
